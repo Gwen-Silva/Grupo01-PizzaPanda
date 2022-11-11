5475b0d87d6936808b0e7550fa5bbbf3d8d02d51
KillMuffin <66225077+KillMuffin@users.noreply.github.com>
2022-11-11 15:28:00 -0300

    Spawn de inimigos (temporário) + colisão inimigo e projétil
    + Spawn de inimigo em lanes aleatórias
    + Inimigo removido em colisão com o projétil
    anotação: mecanica de spawn inconsistente, necessita ser melhorada ou substituida

--- JOGO-1SEM/TesteJanela.py
+++ JOGO-1SEM/TesteJanela.py
@@ -1,7 +1,7 @@
 import os
-from tkinter import W
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
@@ -15,8 +15,6 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 FPS = 60
-VEL = 5
-PROJECTILE_VEL = 15
 CLOCK = pygame.time.Clock()
 
 DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT = (64, 64)
@@ -45,75 +43,77 @@ MAIN_CHARACTER = pygame.transform.scale(
     MAIN_CHARACTER, (DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT)
 )
 
-TEST_DUMMY = pygame.image.load(
-    os.path.join('JOGO-1SEM', 'Assets', 'panda.jpg')
-)
-TEST_DUMMY = pygame.transform.scale(TEST_DUMMY, (DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT))
-
 PIZZA_PEPPERONI = pygame.image.load(
-    os.path.join('JOGO-1SEM','Assets', 'Side_pizza.png ')
+    os.path.join('JOGO-1SEM','Assets', 'Side_pizza_2.png ')
 )
+
 PIZZA_PEPPERONI = pygame.transform.scale(PIZZA_PEPPERONI, (64, 64))
 
 #variáveis dinâmicas
     #hitboxes
 mc_hitbox = pygame.Rect(250, 360, DEFAULT_CHARACTER_WIDHT, DEFAULT_CHARACTER_HEIGHT)
-enemy_hitbox = pygame.Rect(700, 360, DEFAULT_CHARACTER_WIDHT, DEFAULT_CHARACTER_HEIGHT)
 pizza_refill_zone = pygame.Rect(0, ((HEIGHT - (HEIGHT * 0.20) * 3) // 1), (32), (256))
     #Listas
 mc_projectiles = []
-enemy_alive = []
 waves = []
+lanes_pos = [LANE_1.y, LANE_2.y, LANE_3.y, LANE_4.y]
+enemy_alive_pos = []
+enemy_alive_stats = []
+    #inimigos
+enemy_basico_stats = [1, 1]
+enemy_basico_hitbox = pygame.Rect(WIDTH - DEFAULT_CHARACTER_WIDHT, (lanes_pos[random.randint(0, 3)]), 
+DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT)
+    #velocidades
+Vel = 5
+EnemyVel = 10
+ProjectileVel = 10
 #EVENTOS
-enemy_hit = pygame.USEREVENT + 1
+SpawnCliente = pygame.event.custom_type() + 1
 
 #funções
 def movement(keys_pressed, mc_hitbox):
-    if keys_pressed[pygame.K_a] and mc_hitbox.x - VEL > 0:
-            mc_hitbox.x -= VEL
+    if keys_pressed[pygame.K_a] and mc_hitbox.x - Vel > 0:
+            mc_hitbox.x -= Vel
 
-    if keys_pressed[pygame.K_d] and (mc_hitbox.x + DEFAULT_CHARACTER_WIDHT) + VEL < WIDTH // 4:
-            mc_hitbox.x += VEL
+    if keys_pressed[pygame.K_d] and (mc_hitbox.x + DEFAULT_CHARACTER_WIDHT) + Vel < WIDTH // 4:
+            mc_hitbox.x += Vel
 
-    if keys_pressed[pygame.K_w] and mc_hitbox.y - VEL > 0 + ((HEIGHT * 0.20 // 1)):
-            mc_hitbox.y -= VEL
+    if keys_pressed[pygame.K_w] and mc_hitbox.y - Vel > 0 + ((HEIGHT * 0.20 // 1)):
+            mc_hitbox.y -= Vel
 
-    if keys_pressed[pygame.K_s] and mc_hitbox.y + VEL < HEIGHT - DEFAULT_CHARACTER_HEIGHT:
-            mc_hitbox.y += VEL
+    if keys_pressed[pygame.K_s] and mc_hitbox.y + Vel < HEIGHT - DEFAULT_CHARACTER_HEIGHT:
+            mc_hitbox.y += Vel
 
-def pizza_trow(mc_projectiles, projectile):
-        mc_projectiles.append(projectile)
-
-def main():
+def jogo():
     run = True
     while run: 
-        
+        # spawn teste
+        pygame.time.set_timer(SpawnCliente, 18)
+
         keys_pressed = pygame.key.get_pressed()
         movement(keys_pressed, mc_hitbox)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_j:
                     projectile = pygame.Rect(
-                        mc_hitbox.x + mc_hitbox.width, mc_hitbox.y + mc_hitbox.height/2, 50 , 4)
+                        mc_hitbox.x + mc_hitbox.width, mc_hitbox.y + mc_hitbox.height/3 , 50 , 10)
                     mc_projectiles.append(projectile)
 
-        for projectile in mc_projectiles:
-            projectile.x += (PROJECTILE_VEL) // 1
-            if enemy_hitbox.colliderect(projectile):
-                mc_projectiles.remove(projectile)
-            if projectile.x > WIDTH:
-                mc_projectiles.remove(projectile)
-            elif projectile.x < 0: 
-                         mc_projectiles.remove(projectile)
+            if event.type == SpawnCliente:
+                SpawnEnemy()
+
+        ProjectileHandling()
+        EnemyHandling()
+        ColissionHandling()
 
         #UPDATE DA TELA
         
         #preenche a tela
         WIN.blit(BACKGROUND_1, (0, 0))
+
         pygame.draw.rect(WIN, WHITE, LANE_1)
         pygame.draw.rect(WIN, BLACK, LANE_2)
         pygame.draw.rect(WIN, WHITE, LANE_3)
@@ -123,11 +123,12 @@ def main():
         pygame.draw.rect(WIN, BLACK, mc_hitbox)
         WIN.blit(MAIN_CHARACTER, (mc_hitbox.x, mc_hitbox.y))
 
-        pygame.draw.rect(WIN, RED, enemy_hitbox)
-        
+        for enemy_basico_hitbox in enemy_alive_pos:
+            pygame.draw.rect(WIN, RED, enemy_basico_hitbox)  
+
         for projectile in mc_projectiles:
-            WIN.blit(PIZZA_PEPPERONI, (projectile))
             pygame.draw.rect(WIN, RED, projectile)
+            WIN.blit(PIZZA_PEPPERONI, (projectile.x - 4, projectile.y - 36))
 
         pygame.display.update()
 
@@ -135,5 +136,33 @@ def main():
         CLOCK.tick(FPS)
     pygame.quit()
 
-if __name__ == "__main__":
-    main()
+def MainMenu():
+    print("placeholder")
+
+def ProjectileHandling():
+    for projectile in mc_projectiles:
+        projectile.x += (ProjectileVel) // 1
+        if projectile.x > WIDTH:
+            mc_projectiles.remove(projectile)
+        elif projectile.x < 0: 
+             mc_projectiles.remove(projectile)
+
+def EnemyHandling():
+    for enemy_basico_hitbox in enemy_alive_pos:
+        enemy_basico_hitbox.x -= (enemy_basico_stats[1]) // 1
+
+def ColissionHandling():
+    for projectile in mc_projectiles:
+        for enemy_basico_hitbox in enemy_alive_pos:
+            if projectile.colliderect(enemy_basico_hitbox):
+                mc_projectiles.remove(projectile)
+                enemy_alive_pos.remove(enemy_basico_hitbox)
+
+
+def SpawnEnemy():
+    enemy_basico_hitbox = pygame.Rect(WIDTH - DEFAULT_CHARACTER_WIDHT, (lanes_pos[random.randint(0, 3)] + 40), 
+    DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT)
+    enemy_basico_stats = [1, 3]
+    enemy_alive_pos.append(enemy_basico_hitbox)
+    enemy_alive_stats.append(enemy_basico_stats)
+jogo()
