6cc64e395ea9e66d9648b1456012a3b571b01e89
KillMuffin <66225077+KillMuffin@users.noreply.github.com>
2022-11-13 03:42:36 -0300

    Update dos sprites da pizza
    +adição dos assets do drive
    +sprits dos projeteis
    +animação dos projeteis
    ~ajuste da hitbox dos projeteis para refletir os sprits
    -sprites dos projeteis obsoletos removidos

--- JOGO-1SEM/TesteJanela.py
+++ JOGO-1SEM/TesteJanela.py
@@ -35,19 +35,63 @@ LANE_4 = pygame.Rect((WIDTH - ((WIDTH * 0.75) // 1)), (HEIGHT - ((HEIGHT * 0.20)
 BACKGROUND_1 = pygame.image.load(
     os.path.join('JOGO-1SEM', 'Assets', 'Background.jpg')
 )
+BACKGROUND_1 = pygame.transform.scale(BACKGROUND_1, (WIDTH, HEIGHT)
+)
 
 MAIN_CHARACTER = pygame.image.load(
-    os.path.join('JOGO-1SEM','Assets', 'panda.jpg')
+    os.path.join('JOGO-1SEM','Assets', 'Pizza_Panda.png')
 )
 MAIN_CHARACTER = pygame.transform.scale(
     MAIN_CHARACTER, (DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT)
 )
 
-PIZZA_PEPPERONI = pygame.image.load(
-    os.path.join('JOGO-1SEM','Assets', 'Side_pizza_2.png ')
+PIZZA1_0 = pygame.image.load(
+    os.path.join('JOGO-1SEM','Assets', 'Itens', 'Pizzas', '1.png')
 )
 
-PIZZA_PEPPERONI = pygame.transform.scale(PIZZA_PEPPERONI, (64, 64))
+PIZZA1_1 = pygame.transform.rotate(PIZZA1_0, 90)
+
+PIZZA1_2 = pygame.transform.rotate(PIZZA1_0, 180)
+
+PIZZA1_3 = pygame.transform.rotate(PIZZA1_0, 270)
+
+PIZZA1 = [PIZZA1_0, PIZZA1_1, PIZZA1_2, PIZZA1_3]
+
+PIZZA2_0 = pygame.image.load(
+    os.path.join('JOGO-1SEM','Assets', 'Itens', 'Pizzas', '2.png')
+)
+
+PIZZA2_1 = pygame.transform.rotate(PIZZA2_0, 90)
+
+PIZZA2_2 = pygame.transform.rotate(PIZZA2_0, 180)
+
+PIZZA2_3 = pygame.transform.rotate(PIZZA2_0, 270)
+
+PIZZA2 = [PIZZA2_0, PIZZA2_1, PIZZA2_2, PIZZA2_3]
+
+PIZZA3_0 = pygame.image.load(
+    os.path.join('JOGO-1SEM','Assets', 'Itens', 'Pizzas', '3.png')
+)
+
+PIZZA3_1 = pygame.transform.rotate(PIZZA3_0, 90)
+
+PIZZA3_2 = pygame.transform.rotate(PIZZA3_0, 180)
+
+PIZZA3_3 = pygame.transform.rotate(PIZZA3_0, 270)
+
+PIZZA3 = [PIZZA3_0, PIZZA3_1, PIZZA3_2, PIZZA3_3]
+
+PIZZA4_0 = pygame.image.load(
+    os.path.join('JOGO-1SEM','Assets', 'Itens', 'Pizzas', '4.png')
+)
+
+PIZZA4_1 = pygame.transform.rotate(PIZZA4_0, 90)
+
+PIZZA4_2 = pygame.transform.rotate(PIZZA4_0, 180)
+
+PIZZA4_3 = pygame.transform.rotate(PIZZA4_0, 270)
+
+PIZZA4 = [PIZZA4_0, PIZZA4_1, PIZZA4_2, PIZZA4_3]
 
 #variáveis dinâmicas
 playing = True
@@ -58,6 +102,7 @@ mc_hitbox = pygame.Rect(250, 360, DEFAULT_CHARACTER_WIDHT, DEFAULT_CHARACTER_HEI
 pizza_refill_zone = pygame.Rect(0, ((HEIGHT - (HEIGHT * 0.20) * 3) // 1), (32), (256))
     #Listas
 mc_projectiles = []
+mc_projectiles_type = []
 lanes_pos = [LANE_1.y, LANE_2.y, LANE_3.y, LANE_4.y]
 enemy_alive_pos = []
 enemy_alive_stats = []
@@ -67,7 +112,7 @@ enemy_basico_hitbox = pygame.Rect(WIDTH - DEFAULT_CHARACTER_WIDHT, (lanes_pos[ra
 DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT)
     #velocidades
 vel = 5
-projectile_vel = 10
+projectile_vel = 3
 #EVENTOS
 SpawnClient = pygame.event.custom_type() + 1
 SpawnClientEvent = pygame.event.Event(SpawnClient)
@@ -87,16 +132,23 @@ def movement(keys_pressed, mc_hitbox):
             mc_hitbox.y += vel
 
 def Jogo():
+    all_pizzas = [PIZZA1, PIZZA2, PIZZA3, PIZZA4]
+    sprite_order = 0
     point_time = 0
+    point_time1 = 0
     run = True
     while run: 
-        #spawn teste
+
         if playing == True:
             run_time = pygame.time.get_ticks()
         
         if run_time - point_time > 2000:
             point_time = run_time
             pygame.event.post(SpawnClientEvent)
+
+        if run_time - point_time1 > 100:
+            point_time1 = run_time
+            sprite_order += 1
 
         keys_pressed = pygame.key.get_pressed()
         movement(keys_pressed, mc_hitbox)
@@ -107,9 +159,11 @@ def Jogo():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_j:
                     projectile = pygame.Rect(
-                        mc_hitbox.x + mc_hitbox.width, mc_hitbox.y + mc_hitbox.height/3 , 50 , 10)
+                        mc_hitbox.x + mc_hitbox.width, mc_hitbox.y + mc_hitbox.height/8 , 40 , 40)
                     mc_projectiles.append(projectile)
-                
+                    projectile_type = all_pizzas[random.randint(0, len(all_pizzas) - 1)]
+                    mc_projectiles_type.append(projectile_type)
+
             if event == SpawnClientEvent:
                 SpawnEnemy()
 
@@ -122,25 +176,30 @@ def Jogo():
         
         #preenche a tela
         WIN.blit(BACKGROUND_1, (0, 0))
+        show_hitboxes = True
+        if show_hitboxes == True:
+            pygame.draw.rect(WIN, WHITE, LANE_1)
+            pygame.draw.rect(WIN, BLACK, LANE_2)
+            pygame.draw.rect(WIN, WHITE, LANE_3)
+            pygame.draw.rect(WIN, BLACK, LANE_4)
+            pygame.draw.rect(WIN, BLACK, pizza_refill_zone)
 
-        pygame.draw.rect(WIN, WHITE, LANE_1)
-        pygame.draw.rect(WIN, BLACK, LANE_2)
-        pygame.draw.rect(WIN, WHITE, LANE_3)
-        pygame.draw.rect(WIN, BLACK, LANE_4)
-        pygame.draw.rect(WIN, BLACK, pizza_refill_zone)
+            pygame.draw.rect(WIN, BLACK, mc_hitbox)
 
-        pygame.draw.rect(WIN, BLACK, mc_hitbox)
+            for enemy_basico_hitbox in enemy_alive_pos:
+                pygame.draw.rect(WIN, RED, enemy_basico_hitbox)  
+
+            for projectile in mc_projectiles:
+                pygame.draw.rect(WIN, RED, projectile)
+        
+        for projectile in mc_projectiles:
+            WIN.blit(all_pizzas[projectile[1] % 4][(sprite_order % 4)], (projectile.x - 10, projectile.y - 12))
+
         WIN.blit(MAIN_CHARACTER, (mc_hitbox.x, mc_hitbox.y))
 
-        for enemy_basico_hitbox in enemy_alive_pos:
-            pygame.draw.rect(WIN, RED, enemy_basico_hitbox)  
-
-        for projectile in mc_projectiles:
-            pygame.draw.rect(WIN, RED, projectile)
-            WIN.blit(PIZZA_PEPPERONI, (projectile.x - 4, projectile.y - 36))
-
         pygame.display.update()
-
+        
+        print(PIZZA1)
 
         CLOCK.tick(FPS)
     pygame.quit()
@@ -169,7 +228,7 @@ def ColissionHandling():
 
 
 def SpawnEnemy():
-    enemy_basico_hitbox = pygame.Rect(WIDTH - DEFAULT_CHARACTER_WIDHT, (lanes_pos[random.randint(0, 3)] + 40), 
+    enemy_basico_hitbox = pygame.Rect(WIDTH + DEFAULT_CHARACTER_WIDHT, (lanes_pos[random.randint(0, 3)] + 40), 
     DEFAULT_CHARACTER_HEIGHT, DEFAULT_CHARACTER_WIDHT)
     enemy_basico_stats = [1, 3]
     enemy_alive_pos.append(enemy_basico_hitbox)
